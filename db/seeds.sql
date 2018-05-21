9a1e2aa98311218c683dee3ddc290f71e4929328
doreensheen <sheen.doreen@gmail.com>
2018-05-21 12:49:58 -0500

    database created

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Mac and Cheese Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon-Bacon Cheese Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Mushroom Burger", true);
+
